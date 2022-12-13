9ad9888108ae852eb556e8e09aa20ea69d70a732
pnbakken <74681160+pnbakken@users.noreply.github.com>
2022-12-13 09:42:58 +0100

    Trying something different

--- components/new-projects-display/index.tsx
+++ components/new-projects-display/index.tsx
@@ -1,33 +1,12 @@
 import { useState } from "react";
-import styles from "./index.module.scss";
+import { projects } from "../../pages/api/work";
 
 const NewProjectsDisplay = ({ lang }) => {
-  const displayTab = () => {};
-  const [selected, setSelected] = useState(null);
-  const handleSelected = (key) => setSelected(key);
-
+  const [selectedProject, setSelectedProject] = useState(null);
   return (
     <div className="new-projects-display full-width">
       <div className={"projects full-width"}>
-        <ul
-          className={`projects-list no-list-style flex-r justify-around full-width`}
-        >
-          <Project
-            handleSelected={handleSelected}
-            key={1}
-            selected={selected === 1 ? true : false}
-          />
-          <Project
-            handleSelected={handleSelected}
-            key={2}
-            selected={selected === 2 ? true : false}
-          />
-          <Project
-            handleSelected={handleSelected}
-            key={3}
-            selected={selected === 3 ? true : false}
-          />
-        </ul>
+        <Tabs data={projects} handleSelected={setSelectedProject} />
       </div>
     </div>
   );
@@ -35,21 +14,10 @@ const NewProjectsDisplay = ({ lang }) => {
 
 export default NewProjectsDisplay;
 
-const Project = ({ project = null, handleSelected, selected, key }) => {
-  return (
-    <li className="project flex-r justify-center align-center">
-      <div
-        className="project-tab full-width flex-c align-center"
-        onClick={() => {
-          handleSelected(key);
-        }}
-      >
-        <div className="tab-text">Project Title</div>
-      </div>
-      <div className={`project-body full-width ${selected && "selected"}`}>
-        This is a project that I did to do a project and make it nice. And then
-        I wrote some words about it
-      </div>
-    </li>
-  );
-};
+function Tabs({ data, handleSelected }) {
+  return <div className="tabs"></div>;
+}
+
+function Project({ data }) {
+  return <div className="project"></div>;
+}
